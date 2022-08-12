f21600393f38ba1ba1d37eb1754fe661be9a7312
o.nikolaev <o.nikolaev@rtinform.ru>
2022-08-12 09:30:53 +0300

    Structure programming

---
+++ 04_struct_programming.py
@@ -0,0 +1,13 @@
+# Структурное программирование
+import ezgraphics as gr
+
+def build_house(window, x, y, width):
+    """Функция, которая рисует дом"""
+    canvas = window.canvas()
+    canvas.drawRectangle(x, y, width, width)
+    window.wait()
+
+win = gr.GraphicsWindow(300, 300)
+win.setTitle("Russian game")
+
+build_house(win, 100, 100, 100)
