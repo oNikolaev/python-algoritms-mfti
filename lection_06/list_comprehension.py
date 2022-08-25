6c47740dcbed77e533c7d4595f49dbc0605871aa
o.nikolaev <o.nikolaev@rtinform.ru>
2022-08-26 02:12:46 +0300

    List examples, list comprehension tools

---
+++ lection_06/list_comprehension.py
@@ -0,0 +1,8 @@
+A = [x ** 2 for x in range(10)]
+print(A)
+
+B = [x ** 2 for x in A if x % 2 == 0]
+print(B)
+
+C = [(0 if x < 0 else x ** 2) for x in A if x % 2 == 0]
+print(C)
---
+++ lection_06/list_demo.py
@@ -0,0 +1,6 @@
+A = []
+x = int(input("Введите число: "))
+A.append(x)
+print(len(A))
+x = A.pop()
+print(x)
